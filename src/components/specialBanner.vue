25ae6ef8f4b7ec582ed0c9139501d9553a278997
Julian.Hartwig <Julian.Hartwig@htwg-konstanz.de>
2025-11-24 19:56:07 +0100

    Iteration 7

---
+++ src/components/specialBanner.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useBannerStore } from '@/stores/banner';
+import Button from '@/components/button.vue';
+
+const bannerStore = useBannerStore();
+
+defineProps({
+    text: {
+        type: String,
+        default: 'Viele Sonderangebote heute!',
+    }
+});
+</script>
+
+<template>
+    <div id="specialbanner" class="text-center py-3" v-if="bannerStore.isVisible">
+        {{ text }}&nbsp;&nbsp;
+        <Button variant="dark" @click="bannerStore.hideBanner">OK!</Button>
+    </div>
+</template>
