b312907728a779ba5d8186103c990593d93e1b1f
Julian.Hartwig <you@example.com>
2025-12-25 10:27:05 +0100

    Iteration 13a

--- src/main.js
+++ src/main.js
@@ -6,7 +6,17 @@ import { createPinia } from 'pinia'
 
 const pinia = createPinia()
 
+const auth0 = createAuth0({
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+    authorizationParams: {
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+        redirect_uri: window.location.origin
+    }
+})
+
 createApp(App)
     .use(router)
     .use(pinia)
+    .use(auth0)
     .mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import TaskCatalog from '@/views/taskCatalog.vue';   
 import TaskDetail from '@/views/taskDetail.vue';    
-import Login from '@/views/login.vue';             
+import Login from '@/views/login.vue';
+import Profile from '@/views/Profile.vue';             
 import Dashboard from '@/views/dashboard.vue';       
 
 const routes = [
@@ -30,6 +31,7 @@ const routes = [
     component: Login,
     meta: { hideLoginButton: true },
   },
+  { path: '/profile', component: Profile },
   {
     path: '/dashboard',
     name: 'dashboard',
