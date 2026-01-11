c6443fdfbd2649d4bec50f9bca034e51251e58b9
Julian.Hartwig <you@example.com>
2026-01-11 22:17:59 +0100

    es funktioniert endlich mit db!!!!!!

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { authGuard } from '@auth0/auth0-vue'
-
 import Home from '@/views/Home.vue'
 import TaskCatalog from '@/views/taskCatalog.vue'
 import TaskDetail from '@/views/taskDetail.vue'
@@ -13,49 +12,33 @@ const routes = [
     name: 'home',
     component: Home
   },
-
   {
     path: '/dashboard',
     name: 'dashboard',
     component: Dashboard,
-    meta: {
-      requiresAuth: true,
-      hideLoginButton: true
-    },
+    meta: { requiresAuth: true },
     beforeEnter: authGuard
   },
-
   {
     path: '/tasks',
     name: 'tasks',
     component: TaskCatalog,
-    meta: {
-      requiresAuth: true,
-      hideLoginButton: true
-    },
+    meta: { requiresAuth: true },
     beforeEnter: authGuard
   },
-
   {
     path: '/task/:id',
     name: 'task-detail',
     component: TaskDetail,
     props: true,
-    meta: {
-      requiresAuth: true,
-      hideLoginButton: true
-    },
+    meta: { requiresAuth: true },
     beforeEnter: authGuard
   },
-
   {
     path: '/profile',
     name: 'profile',
     component: Profile,
-    meta: {
-      requiresAuth: true,
-      hideLoginButton: true
-    },
+    meta: { requiresAuth: true },
     beforeEnter: authGuard
   }
 ]
