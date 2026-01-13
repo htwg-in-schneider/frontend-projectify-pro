6ae2666629ac739bddd8866198a45db5d4c2a505
katarinastevanovic7 <katarina.stevanovic7676@gmail.com>
2026-01-13 19:36:05 +0100

    main

--- src/main.js
+++ src/main.js
@@ -20,8 +20,8 @@ const pinia = createPinia()
     clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
     authorizationParams: {
       audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-      //redirect_uri: window.location.origin + window.location.pathname
-      redirect_uri: "https://htwg-in-schneider.github.io/frontend-projectify-pro/"
+      redirect_uri: window.location.origin + window.location.pathname
+     // redirect_uri: "https://htwg-in-schneider.github.io/frontend-projectify-pro/"
     },
 
     cacheLocation: 'localstorage',
